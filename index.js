2284975d3b8477f77970ff5d9bdbff04493fcb98
Greg Stimson <stimsong@yahoo.com>
2018-01-11 17:00:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string === lowerCase)
-  return ("I can't hear you!")    // whisper
+  return sayHiToGrandma("I can't hear you!")    // whisper
   else
   if (string === upperCase)
   return ("YES INDEED!")    // shout
