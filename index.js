88fbb0a8a0887dd08a523d5a3e598fab7d58feb2
Greg Stimson <stimsong@yahoo.com>
2018-01-11 17:19:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (sayHiToGrandma === lowercase) {
+  if (sayHiToGrandma(string) === lowercase) {
   return "I can\'t hear you!"
   }
   else if (string === uppercase) {
