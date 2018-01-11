b5e1706d1d754d695c421867542d0c2622395651
Greg Stimson <stimsong@yahoo.com>
2018-01-11 17:32:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
   return "I can\'t hear you!"
   }
-//  else if (string === uppercase) {
-//  return "YES INDEED!"
-//  }
+  else if (string === string.toUpperCase()) {
+  return "YES INDEED!"
+  }
 //  else if (string === mixedCase) {
 //  return "I love you, too."
 //  }
