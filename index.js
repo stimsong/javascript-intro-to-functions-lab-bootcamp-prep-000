bc0f74763d85bd3214e9ec4b05de27e5f5105cfa
Greg Stimson <stimsong@yahoo.com>
2018-01-11 17:35:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var uppercase = "HELLO!"
-uppercase.toUpperCase() === uppercase  // true
+// var uppercase = "HELLO!"
+// uppercase.toUpperCase() === uppercase  // true
 
-var lowercase = "hello!"
-lowercase.toLowerCase() === lowercase  // true
+// var lowercase = "hello!"
+// lowercase.toLowerCase() === lowercase  // true
 
-var mixedCase = "Hi there!"
-mixedCase.toLowerCase() === mixedCase  // false
-mixedCase.toUpperCase() === mixedCase  // false
+// var mixedCase = "Hi there!"
+// mixedCase.toLowerCase() === mixedCase  // false
+// mixedCase.toUpperCase() === mixedCase  // false
 
 function shout(string) {
   return string.toUpperCase()
